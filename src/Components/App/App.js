f84ece5836e655c65b3c0364d8b2e90801c4d3ec
titanic00 <98103546+titanic00@users.noreply.github.com>
2022-04-18 20:02:32 +0300

    added the second field for parsed text

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,13 +4,16 @@ import { ParsedInput } from "../ParsedInput/ParsedInput";
 
 function App() {
   const [text, setText] = useState("");
-  let [buttonIsClicked, setStyle] = useState(false);
+  // is button clicked in this component
+  const [buttonIsClicked, setStyle] = useState(false);
+  // is button clicked in the ParsedInput component
+  const [isHidden, hiddenState] = useState(false);
 
   // initial style of textarea and div with button
   const style = {
     width: "30%",
     textarea: {
-      height: "900px"
+      height: "900px",
     },
   };
 
@@ -20,42 +23,55 @@ function App() {
 
   // If button is pressed, we change thhe style of the elements
   const handleClick = () => {
-      if(buttonIsClicked) {
-        setStyle(false);
-        return;
-      }
-    buttonIsClicked = setStyle(true);
+    if (buttonIsClicked) {
+      setStyle(false);
+      return;
+    }
+    setStyle(true);
+  };
+
+  const hideInputField = () => {
+    if (isHidden) {
+      hiddenState(false);
+      return;
+    }
+    console.log("hehe");
+    hiddenState(true);
   };
 
   return (
-    <div id="input-field">
-      <div
-        id="switch"
-        style={{
-          width: "30%"
-        }}
-      >
-        <button className="button" onClick={handleClick}>
-          Click
-        </button>
+    <div>
+      <div id="input-field" style={{ display: isHidden ? "none" : "block" }}>
+        <div className="switch">
+          <button className="button" onClick={handleClick}>
+            <i
+              className={buttonIsClicked ? "fas fa-compress" : "fa fa-compress"}
+            ></i>
+          </button>
+        </div>
+        <div className="text-input">
+          <textarea
+            type="text"
+            rows="5"
+            cols="30"
+            onChange={handleChange}
+            value={text}
+            // style depends on if the button is pressed
+            style={{
+              height: buttonIsClicked ? "100px" : style.textarea.height,
+              width: "30%",
+            }}
+          />
+        </div>
       </div>
-      <div className="text-input">
-        <textarea
-          type="text"
-          rows="5"
-          cols="30"
-          onChange={handleChange}
-          value={text}
-          // style depends on if the button is pressed
-          style={{
-            height: buttonIsClicked
-            ? "100px"
-            : style.textarea.height,
-            width: "30%"
-          }}
+      {buttonIsClicked && (
+        <ParsedInput
+          style={style}
+          text={text}
+          hideInput={hideInputField}
+          isInputHidden={isHidden}
         />
-      </div>
-      {buttonIsClicked && <ParsedInput style={style} text={text} />}
+      )}
     </div>
   );
 }
--- src/Components/ParsedInput/ParsedInput.js
+++ src/Components/ParsedInput/ParsedInput.js
@@ -1,12 +1,35 @@
 import React from "react";
 import "./ParsedInput.css";
+import { marked } from "marked";
 
+marked.setOptions({
+  breaks: true,
+});
+
+const renderer = new marked.Renderer();
 
 export function ParsedInput(props) {
+    console.log(props)
 
-    return (
-        <div className="text-input">
-            <textarea id="text-parsed-input" type="text" value={props.text} style={{width: props.style.width}} />
-        </div>
-    )
+  return (
+    <div id="parsed-input">
+      <div className="switch second-section">
+        <button className="button" onClick={props.hideInput}>
+        <i
+              className={props.isInputHidden ? "fa fa-compress" : "fas fa-compress"}
+            ></i>
+        </button>
+      </div>
+      <div
+        id="text-parsed-input"
+        style={{
+        height: props.isInputHidden
+        ? props.style.textarea.height
+        : "765px" }}
+        dangerouslySetInnerHTML={{
+          __html: marked(props.text, { renderer: renderer }),
+        }}
+      />
+    </div>
+  );
 }
